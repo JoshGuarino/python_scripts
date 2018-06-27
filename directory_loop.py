9eb68d447c70655eba96aec255f3f1b0ccfd0b71
JoshGuarino <guar0638@fredonia.edu>
2018-06-27 19:22:32 -0400

    fixed bug for directory loop should be working properly now

--- directory_loop.py
+++ directory_loop.py
@@ -1,34 +1,29 @@
 import os
-
-path = "C:\\Users\\Josh\\Documents\\programs\\test"
+path = "C:\\Users\\Josh\\Documents\\programs\\code_breaker"
 cur_dir = os.listdir(path)
 print(path)
 
 def nav_subdir(cur_path, path_spacing):
     new_dir = os.listdir(cur_path)
-    path_spacing = path_spacing + "         " 
+    path_spacing = path_spacing + "      " 
     for file_name2 in new_dir:
-        if "." not in file_name2:
-            path_len = len(cur_path)
-            print(path_spacing + "-" + file_name2)
-            cur_path = cur_path + "\\" + file_name2
-            nav_subdir(cur_path, path_spacing)
+        path_len = len(cur_path)
+        cur_path = cur_path + "\\" + file_name2
+        if os.path.isfile(cur_path):
+            print(path_spacing + file_name2)
             trim_len = len(cur_path) - path_len
             cur_path = cur_path[:-trim_len]
         else:
-            print(path_spacing + file_name2)
-            
+            print(path_spacing + "-" + file_name2)
+            nav_subdir(cur_path, path_spacing)
+            trim_len = len(cur_path) - path_len
+            cur_path = cur_path[:-trim_len]
 
 for file_name in cur_dir:
     path_spacing = ""
     cur_path = path + "\\" + file_name
-    if "." not in file_name:
+    if os.path.isfile(cur_path):
+        print(file_name)
+    else:
         print("-" + file_name)
         nav_subdir(cur_path, path_spacing)
-    else:
-        print(file_name)
-
-
-
-    
-        
